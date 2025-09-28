68454ace350726be4af65097fbe693a9ae7832b7
Maianh277-25 <manhd27725@gmail.com>
2025-09-28 16:05:39 +0700

    index.php

---
+++ controllers/homepage.controllers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> </title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+    <div class="wrapper">
+        <?php
+include("../controller/homepagecontrollers/menu.php");
+include("../controller/homepagecontrollers/thanhtimkiem.php");
+include("../controller/homepagecontrollers/meiianh.php")
+        ?>
+</div>
+    </body>
+</html>
+
